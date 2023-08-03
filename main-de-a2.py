7e66b5342393b2a236b6ae9cb8354f69a23a8f62
Pavel Agurov <agurov.pavel@gmail.com>
2023-08-03 23:18:57 +0200

    add lang selection

--- main-de-a2.py
+++ main-de-a2.py
@@ -14,22 +14,21 @@ OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 SENTENCE_SUFFIX_LIST = ['.', '!', '?']
 
-LEVEL = "A2"
-
 how_it_work = """\
 Enter your translation of proposed text, click Check and wait for Gpt validation and advice. 
 """
 
 generation_template = """/
-Hello! I am learning German. My level is {level}.
-Make me a sentence in Russian for translation into German and translate all the words in it, but not the sentence itself.
+Hello! I am learning {lang}. My level is {level}.
+Make me a complicated enough for my level a sentence in Russian for translation into {lang} and translate all the words in it, 
+but not the sentence itself.
 Use this {random} value for seed randomization and generate different sentences.
 Provide answer in JSON format:
 {{
-    "russian_sentence" : "proposed sentence in Russian"
+    "russian_sentence" : "proposed sentence in Russian",
     "words":[
-        ["word": "word in infinitive form", "translation" : "translation into German"}},
-        ["word": "word in infinitive form", "translation" : "translation into German"}}
+        ["word": "word in infinitive form", "translation" : "translation into {lang}"}},
+        ["word": "word in infinitive form", "translation" : "translation into {lang}"}}
     ]
 
 }}
@@ -37,17 +36,19 @@ Provide answer in JSON format:
 """
 
 check_prompt_template = """/
-Hi, I want to check my German.
-I have translation of sentence in Russian (separated by XML tags) to German (separated by XML tags).
-Please correct me if translation is wrong and if there are errors please explain me step by step why and what is wrong.
-Please provide as much detail as possible about the mistakes according to the German grammar, but only really relevant to the mistakes made.
-All explanations should be translated into Russian.
+Hi, I want to check my {lang}.
+I have translation of Russian sentence (separated by XML tags) into {lang} (separated by XML tags).
+Please correct me if my translation is wrong and if there are errors please explain me step by step why and what is wrong.
+Please provide as much detail as possible about the mistakes according to the {lang} grammar, but only really relevant to the mistakes made.
+Do not check original sentence, only check my translation.
+All explanations should be provided in Russian.
 
 Provide answer in JSON format:
 {{
-    "correct" : "correct sentence in German",
+    "correct" : "correct sentence in {lang}",
     "errors_explanations":[
-        "russian explanation 1", "russian explanation 2"
+        ["wrong word in {lang}", "russian detailed explanation of this mistake"],
+        ["wrong word in {lang}", "russian detailed explanation of this mistake"]
     ]
 
 }}
@@ -92,16 +93,17 @@ def on_next_button_click():
     st.session_state[SESSION_SAVED_WORDS_DF] = None
     st.session_state.user_input = ""
 
-st.set_page_config(page_title="Gpt German Trainer")
-st.title("Gpt German Trainer")
+st.set_page_config(page_title="Gpt Language Trainer")
+st.title("Gpt Language Trainer")
 
 header_container   = st.container()
 status_container   = st.empty()
 original_container = st.empty()
 input_container    = st.container()
-user_input = input_container.text_area("Text: ", key="user_input", on_change=on_check_button_click)
+user_input = input_container.text_area("Your translation: ", key="user_input", on_change=on_check_button_click)
 correct_container  = st.container().empty()
 explain_container  = st.empty()
+_  = st.empty().markdown("<hr/>", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
     check_button = st.button(label= "Check", on_click= on_check_button_click, key="check_button")
@@ -109,7 +111,10 @@ with col2:
     next_button  = st.button(label= "Next" , on_click= on_next_button_click , key="next_button" )
 
 with st.sidebar:
+    lang_input = st.text_input("Language: ", key="language", value = "German")
+    leve_intput = st.selectbox("Level:", key="level", options=["Newcomer", "Novice", "Advanced"], index=2)
     words_container = st.expander(label="Help me with words")
+    error_container = st.empty()
 
 header_container.markdown(how_it_work, unsafe_allow_html=True)
 
@@ -152,17 +157,20 @@ generated_russian_sentence = st.session_state[SESSION_SAVED_SENTENCE]
 # not generated yet and it's not checking
 if not generated_russian_sentence and not run_check:
     status_container.markdown('Generate sentence...')
-    russian_sentence_result = generation_chain.run(level = LEVEL, random = now_str)
+    russian_sentence_result = generation_chain.run(level = leve_intput, lang = lang_input, random = now_str)
     status_container.markdown(' ')
-    russian_sentence_json = json.loads(get_fixed_json(russian_sentence_result))
-    russian_sentence = russian_sentence_json['russian_sentence']
-    russian_words    = russian_sentence_json['words']
-    russian_words_list = []
-    for w in russian_words:
-        russian_words_list.append([w["word"], w["translation"]])
-    df_words = pd.DataFrame(russian_words_list, columns= ['Word', 'Translation'])
-    st.session_state[SESSION_SAVED_WORDS_DF] = df_words
-    st.session_state[SESSION_SAVED_SENTENCE] = russian_sentence
+    try:
+        russian_sentence_json = json.loads(get_fixed_json(russian_sentence_result))
+        russian_sentence = russian_sentence_json['russian_sentence']
+        russian_words    = russian_sentence_json['words']
+        russian_words_list = []
+        for w in russian_words:
+            russian_words_list.append([w["word"], w["translation"]])
+        df_words = pd.DataFrame(russian_words_list, columns= ['Word', 'Translation'])
+        st.session_state[SESSION_SAVED_WORDS_DF] = df_words
+        st.session_state[SESSION_SAVED_SENTENCE] = russian_sentence
+    except Exception as error:
+        error_container.markdown(f'{russian_sentence_result}\n{error}')
 
 words_container.dataframe(st.session_state[SESSION_SAVED_WORDS_DF], use_container_width=True, hide_index=True)
 original_container.markdown(st.session_state[SESSION_SAVED_SENTENCE])
@@ -170,7 +178,11 @@ original_container.markdown(st.session_state[SESSION_SAVED_SENTENCE])
 # if we have user input and it's checking - run validation
 if saved_user_input and run_check:
     status_container.markdown('Validation...')
-    validation_result = validation_chain.run(russian_sentence= st.session_state[SESSION_SAVED_SENTENCE],  translation= saved_user_input)
+    validation_result = validation_chain.run(
+            russian_sentence= st.session_state[SESSION_SAVED_SENTENCE],  
+            translation= saved_user_input, 
+            lang = lang_input
+    )
     status_container.markdown(' ')
 
     try:
@@ -203,10 +215,7 @@ if correct and saved_user_input:
     correct_container.markdown(diff.output_markdown, unsafe_allow_html=True)
 
     if len(explanation) > 0:
-        if isinstance(explanation, str):
-            result_rus = f'<li>{explanation}</li>'
-        else:
-            result_rus = '\n'.join([f'<li>{e}</li>' for e in explanation ])
+        result_rus = '\n'.join([f'<li>{e[1]}</li>' for e in explanation])
         explain_container.markdown(result_rus, unsafe_allow_html=True)
     else:
         explain_container.markdown("Предложение верное", unsafe_allow_html=True)
